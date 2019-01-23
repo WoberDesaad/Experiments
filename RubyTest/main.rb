3bc443489e02b2a4a7a682d36717513e918e5e6a
woberdesaad <appelraptormann@gmail.com>
2019-01-23 19:45:25 +0000

    Learning things about Ruby

---
+++ RubyTest/main.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+
+require './test.rb'
+require './test2.rb'
+
+
+TEST.Run
---
+++ RubyTest/test2.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+
+class TEST
+	def self.Run()
+		puts "FRED"
+	end
+end
